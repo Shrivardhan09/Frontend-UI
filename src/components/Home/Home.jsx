71c078c53251ddb3615733e310c140ae7b1fc1bf
Shrivardhan Gopishetty <gopishettyshrivardhan@gmail.com>
2023-09-06 00:16:06 +0530

    home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className='m-5 flex items-center justify-center'>
             <div className='rounded-lg bg-primary-100 px-2  w-5/6 flex items-center justify-center'>
-                <img src="./Home-img.png" alt="frontend-ui" className='' />
+                <img src="./Home-img.png" alt="frontend-ui" />
             </div>
         </div>
     )
