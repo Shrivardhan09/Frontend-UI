e8c2837714679fc90f8792bcf694a3c27c85f5cc
Shrivardhan Gopishetty <gopishettyshrivardhan@gmail.com>
2023-09-06 14:51:48 +0530

    slider page

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,105 @@
+import { motion, useAnimation } from 'framer-motion'
+import '/src/app/globals.css'
+import { useEffect, useState } from 'react';
+
+
+const Slider = () => {
+
+    return (
+        <>
+            <motion.div
+                initial='hidden'
+                whileInView='visible'
+                transition={{ duration: 1.5 }}
+                variants={{
+                    hidden: { opacity: 0, x: -100, y: -25 },
+                    visible: { opacity: 1, x: 0, y: 0 }
+                }}
+            >
+                <div className='flex justify-center items-center'>
+                    <div className=' flex gap-3 items-center w-4/5'>
+                        <h2 className='text-4xl font-bold'>Does it sound familiar</h2>
+                        <img src="./span.png" alt="angry-jellyfish" className='w-8' />
+                    </div>
+                </div>
+            </motion.div>
+
+
+            <div
+                className='flex justify-center items-center gap-7 overflow-x-hidden overflow-y-hidden flex-nowrap'
+
+            >
+                <motion.div
+                    className='flex justify-center items-center gap-7 m-7 p-7'
+                    initial='hidden'
+                    whileInView='visible'
+                    transition={{ duration: 1.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: 100 },
+                        visible: { opacity: 1, x: -150 }
+                    }}
+                >
+                    <motion.div className='bg-cyan-100 flex flex-col justify-center items-start gap-1 px-5 py-5 rounded-xl card hover:-rotate-[10deg]'>
+                        <div className='w-[25px]'>
+                            <img src="./angryemoji.png" alt="angry-emoji" />
+                        </div>
+                        <div className='font-montserrat font-semibold'>
+                            You argue with your colleague
+                        </div>
+                        <div>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est iusto modi praesentium recusandae pariatur quo facilis.
+                        </div>
+                    </motion.div>
+                    <motion.div className='bg-indigo-100 flex flex-col justify-center items-start gap-1 px-5 py-5 rounded-xl card hover:-rotate-[10deg]'>
+                        <div className='w-[25px]'>
+                            <img src="./blankemoji.png" alt="blank-emoji" />
+                        </div>
+                        <div className='font-montserrat font-semibold'>
+                            You argue with your colleague
+                        </div>
+                        <div>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est iusto modi praesentium recusandae pariatur quo facilis.
+                        </div>
+                    </motion.div>
+                    <motion.div className='bg-pink-50 flex flex-col justify-center items-start gap-1 px-5 py-5 rounded-xl card  hover:-rotate-[10deg]'>
+                        <div className='w-[25px]'>
+                            <img src="./eyebrow-raisedemoji.png" alt="eyebrow-raised-emoji" />
+                        </div>
+                        <div className='font-montserrat font-semibold'>
+                            You argue with your colleague
+                        </div>
+                        <div>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est iusto modi praesentium recusandae pariatur quo facilis.
+                        </div>
+                    </motion.div>
+                    <motion.div className='bg-yellow-100 flex flex-col justify-center items-start gap-1 px-5 py-5 rounded-xl card hover:-rotate-[10deg]'>
+                        <div className='w-[25px]'>
+                            <img src="./grimacing-faceemoji.png" alt="eyebrow-raised-emoji" />
+                        </div>
+                        <div className='font-montserrat font-semibold'>
+                            You argue with your colleague
+                        </div>
+                        <div>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est iusto modi praesentium recusandae pariatur quo facilis.
+                        </div>
+                    </motion.div>
+                    <motion.div className='bg-sky-200 flex flex-col justify-center items-start gap-1 px-5 py-5 rounded-xl card hover:-rotate-[10deg]'>
+                        <div className='w-[25px]'>
+                            <img src="./unamused-faceemoji.png" alt="eyebrow-raised-emoji" />
+                        </div>
+                        <div className='font-montserrat font-semibold'>
+                            You argue with your colleague
+                        </div>
+                        <div>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est iusto modi praesentium recusandae pariatur quo facilis.
+                        </div>
+                    </motion.div>
+
+                </motion.div>
+            </div>
+
+        </>
+    )
+}
+
+export default Slider
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import Home from '@/components/Home/Home'
 import '/src/app/globals.css'
 import Navbar from '@/components/Navbar/Navbar'
+import Slider from '@/components/Slider/Slider'
 
 export default function Index() {
     return (
         <main className='m-0 '>
             <Navbar />
             <Home />
+            < Slider />
         </main>
 
     )
