77636cbbca64f49babc7d97022e530c918e32302
Shrivardhan Gopishetty <gopishettyshrivardhan@gmail.com>
2023-09-07 20:16:31 +0530

    added spacing css

--- src/components/TimelineAnimation/EqBox.jsx
+++ src/components/TimelineAnimation/EqBox.jsx
@@ -6,11 +6,11 @@ const EqBox = () => {
                 <div className="flex-1">
                     <h2 className="text-3xl font-montserrat font-bold">Be the best with the EQ</h2>
                 </div>
-                <div className="flex-1 text-justify">
+                <div className="flex-1 text-justify tracking-tight">
                     <p>Again, please note that these interpretations are based on the assumption that these values are related to a property called "ease" and are measured on a scale.</p>
                 </div>
-                <div className="flex-1 text-justify">
-                    <p> The actual interpretation may vary depending on the context and the specific meaning assigned to these values in the given dataset or scenario.</p>
+                <div className="flex-1 text-justify tracking-tight ">
+                    <p > The actual interpretation may vary depending on the context and the specific meaning assigned to these values in the given dataset or scenario.</p>
                 </div>
 
             </div>
