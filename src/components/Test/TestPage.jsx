3389fcefbf4c61711f18de1c8a45fb67b99124fb
Shrivardhan Gopishetty <gopishettyshrivardhan@gmail.com>
2023-09-08 09:43:23 +0530

    removed apos

--- src/components/Test/TestPage.jsx
+++ src/components/Test/TestPage.jsx
@@ -7,7 +7,7 @@ const TestPage = () => {
             <div className="w-4/5 flex flex-col items-center justify-center gap-5">
                 <p className="text-lg">We take privacy seriously</p>
                 <h3 className="font-bold font-montserrat text-3xl">Before you get started</h3>
-                <p className="text-lg w-1/2 text-center">We won't share any answers with anyone &#40;and wont every tell you which friends said what about you&#41;</p>
+                <p className="text-lg w-1/2 text-center">We wont share any answers with anyone &#40;and wont every tell you which friends said what about you&#41;</p>
             </div>
             <div className="flex gap-1 items-center justify-center">
                 <p>with Love,</p>
